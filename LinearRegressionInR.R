69fd51dd0d96ef063baa5ac58d73649e77bf24b5
Priyanka Kasture <42208426+priyanka-kasture@users.noreply.github.com>
2018-10-25 12:26:30 +0530

    Update LinearRegressionInR.R

--- LinearRegressionInR.R
+++ LinearRegressionInR.R
@@ -35,6 +35,10 @@ lm <- lm(mpg ~ hp + cyl + wt)
 summary(lm)
 # Compare the Adjusted R-Values, they are same.
 
+# Residual (Error) Plot for the fitted model.
+plot(resid(lm))
+abline(h=0)
+
 Table <- as.data.frame(round(cbind(B,SE,t,p), digits = 3))
 names(Table)[1:4] <- c("Estimate:","Standard Error:","t-value:","p-value:")
 
@@ -43,3 +47,37 @@ Table
 
 # Displaying Adjusted R-Squared.
 adj.R2
+
+# Testing the model by subjecting it new 'wt' values.
+# The model predicts the corresponding 'mpg' values.
+# Compare the values with those in 'mtcars', they are almost the same.
+fitted_model = lm(mpg ~ wt, mtcars)
+newdata = data.frame(wt = runif(10, 1, 5))
+newdata$predicted_mpg = predict(fitted_model, newdata = newdata)
+newdata
+
+# Regression Plot - Horse Power v/s Miles Per Gallon
+ggplot(mtcars, aes(hp, mpg)) + geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  ylab("Miles per Gallon") +
+  xlab("No. of Horsepower") +
+  ggtitle("Impact of Number of Horsepower on MPG")
+
+# The mpg is unlikely to hit zero as the hp increases, we would expect a more asymptotic line. 
+# So I have applied stat_smooth to get a better fit. (Optional)
+ggplot(mtcars, aes(hp, mpg)) +
+  stat_smooth() + geom_point() +
+  ylab("Miles per Gallon") +
+  xlab ("No. of Horsepower") +
+  ggtitle("Impact of Number of Horsepower on MPG")
+
+# Regression Plot - No. of Cylinders v/s Miles Per Gallon
+ggplot(mtcars, aes(cyl, mpg)) + geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  ylab("Miles per Gallon") + xlab("No. of Cylinders") +
+  ggtitle("Impact of Number of Cylinders on MPG")
+
+# Residual (Error) Diagnostics Of Final Model (Optional)
+par(mfrow=c(2,2))
+plot(lm)
+
